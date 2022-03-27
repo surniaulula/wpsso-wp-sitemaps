5358c864f79f07979617d51ab5cbbfb45e07fd2e
JS Morisset <jsm@surniaulula.com>
2022-03-27 12:04:19 -0700

    v3.0.0-dev.5

--- lib/sitemaps/filters.php
+++ lib/sitemaps/filters.php
@@ -257,7 +257,7 @@ if ( ! class_exists( 'WpssoWpsmSitemapsFilters' ) ) {
 				}
 			}
 
-			$sitemap_entry[ 'alternates' ] = $this->p->util->get_alternates( $mod );
+			$sitemap_entry[ 'alternates' ] = $this->p->util->get_link_rel_alternates( $mod );
 
 			return $sitemap_entry;
 		}
@@ -339,7 +339,7 @@ if ( ! class_exists( 'WpssoWpsmSitemapsFilters' ) ) {
 			$mod = $this->p->term->get_mod( $term->term_id );
 
 			$sitemap_entry[ 'language' ]   = $this->p->schema->get_lang( $mod );
-			$sitemap_entry[ 'alternates' ] = $this->p->util->get_alternates( $mod );
+			$sitemap_entry[ 'alternates' ] = $this->p->util->get_link_rel_alternates( $mod );
 
 			return $sitemap_entry;
 		}
