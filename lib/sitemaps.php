10c1243d239ed96b5eea5f99cf179653f3c8f20d
JS Morisset <jsm@surniaulula.com>
2022-10-31 13:32:01 -0700

    v4.1.0-dev.1

--- lib/sitemaps.php
+++ lib/sitemaps.php
@@ -10,16 +10,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
+/**
+ * Since WPSSO WPSM v3.0.0.
+ *
+ * Run wpssowpsm_get_server() to set $wp_sitemaps before the WordPress wp_sitemaps_get_server() function does.
+ *
+ * Note that the 'init' action hook runs before the 'parse_request' action hooks.
+ *
+ * See wordpress/wp-includes/default-filters.php.
+ * See wordpress/wp-includes/sitemaps.php.
+ */
 if ( ! function_exists( 'wpssowpsm_get_server' ) ) {
 
-	/**
-	 * Since WPSSO WPSM v3.0.0.
-	 *
-	 * Run wpssowpsm_get_server() to set $wp_sitemaps before the WordPress wp_sitemaps_get_server() function does.
-	 *
-	 * See wordpress/wp-includes/default-filters.php.
-	 * See wordpress/wp-includes/sitemaps.php.
-	 */
 	add_action( 'init', 'wpssowpsm_get_server', -10 );
 
 	function wpssowpsm_get_server() {
@@ -41,5 +43,32 @@ if ( ! function_exists( 'wpssowpsm_get_server' ) ) {
 		}
 
 		return $wp_sitemaps;
+	}
+}
+
+/**
+ * Since WPSSO WPSM v4.1.0.
+ *
+ * Fix for sitemaps pagination bug.
+ *
+ * Executes before send_headers(), query_posts(), handle_404(), and register_globals().
+ *
+ * See https://wp-kama.com/note/wp-sitemap-bug-404-pagination.
+ * See https://core.trac.wordpress.org/ticket/51912
+ */
+if ( ! function_exists( 'wpssowpsm_wp_query_handle_sitemap' ) ) {
+
+	add_action( 'parse_request', 'wpssowpsm_wp_query_handle_sitemap', -10, 1 );
+
+	function wpssowpsm_wp_query_handle_sitemap( $wp ) {
+
+		if( empty( $wp->query_vars[ 'sitemap' ] ) && empty( $wp->query_vars[ 'sitemap-stylesheet' ] ) ) {	// Nothing to do.
+
+			return;
+		}
+
+		$GLOBALS[ 'wp_query' ]->query_vars = $wp->query_vars;
+
+		wp_sitemaps_get_server()->render_sitemaps();
 	}
 }
