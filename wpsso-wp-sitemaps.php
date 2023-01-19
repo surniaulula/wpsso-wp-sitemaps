98baf6613ca9694e8efa52fb1d436fc1647687e7
JS Morisset <jsm@surniaulula.com>
2023-01-19 14:37:53 -0800

    v5.2.0

--- wpsso-wp-sitemaps.php
+++ wpsso-wp-sitemaps.php
@@ -16,7 +16,7 @@
  * Requires At Least: 5.5
  * Tested Up To: 6.1.1
  * WC Tested Up To: 7.3.0
- * Version: 5.2.0-rc.2
+ * Version: 5.2.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
