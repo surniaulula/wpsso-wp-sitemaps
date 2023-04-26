4e88651e5c3866d6295fe8eca2a9e9fd05f76237
JS Morisset <jsm@surniaulula.com>
2023-04-26 06:45:24 -0700

    v5.4.0

--- wpsso-wp-sitemaps.php
+++ wpsso-wp-sitemaps.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Include Schema images, alternate language URLs, post type archive pages, select post types and taxonomies, exclude "No Index" and redirected pages.
  * Requires Plugins: wpsso
- * Requires PHP: 7.2
+ * Requires PHP: 7.2.5
  * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * WC Tested Up To: 7.6.0
