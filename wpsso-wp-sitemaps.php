9a48c7282618f078a25dcf9017901300629268de
jsmoriss <jsm@surniaulula.com>
2025-12-26 16:46:15 +0000

    v8.4.0

--- wpsso-wp-sitemaps.php
+++ wpsso-wp-sitemaps.php
@@ -13,7 +13,7 @@
  * Description: Improves the WordPress sitemaps XML with article modification times, alternate language URLs, images sitemaps, news sitemaps and more.
  * Requires Plugins: wpsso
  * Requires PHP: 7.4.33
- * Requires At Least: 5.9
+ * Requires At Least: 6.0
  * Tested Up To: 6.9
  * WC Tested Up To: 10.4.3
  * Version: 8.4.0
